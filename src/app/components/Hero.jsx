c9ef65f4fc812b0e9b69f8eba79086b50a52a245
Ali Haider <mirzaaly8@gmail.com>
2025-02-09 03:28:18 +0500

    small changes

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
 
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
-
+    const [isLoading, setisLoading] = useState(true)
     const [loading, setLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
@@ -82,6 +82,14 @@ const Hero = () => {
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden '>
+          {!isLoading && (
+              <div>
+                <div className='three-body' >
+
+                </div>
+                loading
+              </div>
+          )}
             <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
                 <div>
                     <div className='mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg ' >
