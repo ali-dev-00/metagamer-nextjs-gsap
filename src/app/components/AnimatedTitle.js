f9d2690b5e87651320dedeb69ea8ffd45fde4612
Ali Haider <mirzaaly8@gmail.com>
2025-02-09 04:59:15 +0500

    setting up animtion for text

--- src/app/components/AnimatedTitle.js
+++ src/app/components/AnimatedTitle.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const AnimatedTitle = () => {
-  return (
-    <div>AnimatedTitle</div>
-  )
+const AnimatedTitle = ({title,containerClass}) => {
+    const containerRef = useRef(null);
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <div ref={containerRef} className={`animated-title ${containerClass}`} >
+         {title.split('<br/>').map((line,index) => (
+             <div key={index} className='flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3'>
+               {line.split('').map((word,i) => (
+                   <span key={i} className={`animated-word ${containerClass}`} dangerouslySetInnerHTML={{ __html: word }}/>
+               ))}
+             </div>
+         ))}  
+        </div>
+    )
 }
 
 export default AnimatedTitle
