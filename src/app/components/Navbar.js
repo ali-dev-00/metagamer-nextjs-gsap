76f91fc5067b1cd90a2ebbe25ecac01d63e9deda
Ali Haider <mirzaaly8@gmail.com>
2025-02-16 23:41:27 +0500

    setting up navbar component on y axis scroll

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
 import Link from 'next/link';
+import {useWindowScroll} from 'react-use';
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 const Navbar = () => {
@@ -12,6 +13,7 @@ const Navbar = () => {
     const [isIndicatorActive, setisIndicatorActive] = useState(false)
     const navContainerRef = useRef(null);
     const audioElementRef = useRef(null);
+    const {y: currentScrollY  } = useWindowScroll();
     const toggleAudioIndicator = () => {
         setIsAudioPlaying((prev) => !prev);
         setisIndicatorActive((prev) => !prev);
@@ -23,6 +25,10 @@ const Navbar = () => {
             audioElementRef.current.pause();
         }
     }, [isAudioPlaying])
+
+    useEffect(()=>{
+       
+    },[currentScrollY])
     return (
         <div ref={navContainerRef} className='  fixed inset-x-0 top-4 h-16 border-none transition-all duration-700 sm:inset-x-6 z-50'   >
             <header className='absolute top-1/2 w-full -translate-y-1/2 ' >
