7acd15ba7656525aa50b3adce0d4ec8235a61978
Ali Haider <mirzaaly8gmail.com>
2025-03-10 03:01:41 +0500

    createing ref for image animation for story section

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
+import Story from "./components/Story";
 
 const page = () => {
   return (
@@ -11,6 +12,7 @@ const page = () => {
       <Hero />
       <About/>
       <Features/>
+      <Story/>
     </main>
   ) 
 };
