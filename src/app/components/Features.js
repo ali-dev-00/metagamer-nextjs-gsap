5f2bfcb2ce1354509841793fe43e567c8d679877
Ali Haider <mirzaaly8gmail.com>
2025-03-10 02:34:12 +0500

    starting implementing story section

--- src/app/components/Features.js
+++ src/app/components/Features.js
@@ -130,7 +130,7 @@ const Features = () => (
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
             src="videos/feature-2.mp4"
-            title={
+            title={ 
               <>
                 zig<b>m</b>a
               </>
---
+++ src/app/components/Story.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Story = () => {
+  return (
+    <section  id='story' className='min-h-dvh w-screen bg-black text-blude-50' >
+
+    </section>
+  )
+}
+
+export default Story
