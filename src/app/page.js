98399db5693040d62b19f46f6ce03056d8cc8c05
Ali Haider <161706554+ali-dev-00@users.noreply.github.com>
2025-05-06 19:57:08 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,8 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 const page = () => {
   return (
@@ -13,6 +15,8 @@ const page = () => {
       <About/>
       <Features/>
       <Story/>
+      <Contact/>
+      <Footer />
     </main>
   ) 
 };
